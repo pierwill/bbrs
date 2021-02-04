6d52e2228df3c903cdff3a8e93776a03a285924b
pierwill <pierwill@users.noreply.github.com>
2021-02-03 21:07:35 -0800

    snapshot of work

--- src/main.rs
+++ src/main.rs
@@ -8,14 +8,25 @@ use pest::Parser;
 #[grammar = "nb.pest"]
 struct NbParser;
 
+const SEPARATOR: &str = "\n----------------------------------\n";
+
 fn main() {
     let input = "false";
-    let p = NbParser::parse(Rule::term, &input)
+    let p = NbParser::parse(Rule::statement, &input)
         .expect("err")
         .next()
         .unwrap();
     let inner_rules = p.into_inner();
-    println!("\nInner rules:\n{:#?}", inner_rules)
+    println!("Input: \"{}\"\n\n{:#?}", input, inner_rules);
+
+    println!("{}", SEPARATOR);
+
+    let input2 = "t1 = true";
+    let p2 = NbParser::parse(Rule::statement, &input2)
+        .expect("err")
+        .next()
+        .unwrap();
+    println!("Input: \"{}\"\n\n{:#?}", input2, p2);
 }
 
 
@@ -26,16 +37,42 @@ mod tests {
     #[test]
     fn test_term() {
         let input = "true";
-        let p = NbParser::parse(Rule::term, &input)
+        let p = NbParser::parse(Rule::statement, &input)
             .expect("err")
             .next()
             .unwrap();
-        
+
         assert_eq!(p.into_inner().as_str(), "true");
     }
 
     #[test]
-    fn test_if() {
-        let input = "if true then true else false"
+    fn test_term2() {
+        let input = "true";
+        let p = NbParser::parse(Rule::statement, &input)
+            .expect("err")
+            .next()
+            .unwrap();
+        
+        assert_eq!(p.into_inner().next().unwrap().as_rule(), Rule::term);
+    }
+    
+    #[test]
+    fn test_assignment_str() {
+        let input = "t1 = true";
+        let p = NbParser::parse(Rule::statement, &input)
+            .expect("err")
+            .next()
+            .unwrap();
+        assert_eq!(p.into_inner().as_str(), "t1 = true");
+    }
+
+    #[test]
+    fn test_assignment_rule() {
+        let input = "t1 = true";
+        let p = NbParser::parse(Rule::statement, &input)
+            .expect("err")
+            .next()
+            .unwrap();
+        assert_eq!(p.into_inner().next().unwrap().as_rule(), Rule::assignment);
     }
 }
