bd016ffc50685980f09e45c07e8dbd4e34023fde
pierwill <pierwill@users.noreply.github.com>
2021-02-05 10:41:35 -0800

    Format test code

--- src/test.rs
+++ src/test.rs
@@ -6,37 +6,37 @@ use pest::iterators::Pairs;
 
 #[test]
 fn test_parse_true() {
-let src: &str = "true";
-let p = NbParser::parse(Rule::term, &src)
-.expect("err")
-.next()
-.unwrap();
+    let src: &str = "true";
+    let p = NbParser::parse(Rule::term, &src)
+        .expect("err")
+        .next()
+        .unwrap();
 
-let term = parse_term(p);
-assert_eq!(term, Term::TmTrue);
+    let term = parse_term(p);
+    assert_eq!(term, Term::TmTrue);
 }
 
 #[test]
 fn test_parse_false() {
-let src: &str = "false";
-let p = NbParser::parse(Rule::term, &src)
-.expect("err")
-.next()
-.unwrap();
+    let src: &str = "false";
+    let p = NbParser::parse(Rule::term, &src)
+        .expect("err")
+        .next()
+        .unwrap();
 
-let term = parse_term(p);
-assert_eq!(term, Term::TmFalse);
+    let term = parse_term(p);
+    assert_eq!(term, Term::TmFalse);
 }
 
 #[test]
 fn test_parse_if() {
-let src: &str = "if true then true else false";
-let p = NbParser::parse(Rule::term, &src)
-.expect("err")
-.next()
-.unwrap();
+    let src: &str = "if true then true else false";
+    let p = NbParser::parse(Rule::term, &src)
+        .expect("err")
+        .next()
+        .unwrap();
 
-let term = parse_if(Pairs::single(p));
-let want = Some(Term::TmIf(Box::new(Term::TmTrue), Box::new(Term::TmTrue), Box::new(Term::TmFalse)));
-assert_eq!(term, want);
+    let term = parse_if(Pairs::single(p));
+    let want = Some(Term::TmIf(Box::new(Term::TmTrue), Box::new(Term::TmTrue), Box::new(Term::TmFalse)));
+    assert_eq!(term, want);
 }
